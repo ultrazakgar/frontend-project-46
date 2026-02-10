863bf8f7d2e0acea0bfce4bddfd2cd701999c63b
ultrazakgar <zahariavinogr@gmail.com>
2026-02-10 17:46:39 +0300

    step 3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,19 @@
 import { Command } from 'commander';
+import { readFileSync } from 'node:fs';
+import { accessSync, constants } from 'node:fs';
 const program = new Command();
+
+function readfile(filename){
+        let valid = true;
+        try{ // testing
+            accessSync(filename, constants.R_OK)
+        } catch (err){
+            valid = false;
+            console.log (err.message);
+            return null;
+        }
+        return readFileSync(filename);
+}
 
 program
     .name('gendiff')
@@ -14,11 +28,24 @@ program
     //.option('-V, --version', 'output the version number')
     .option('-f, --format [type]','output format')
     .option('-h, --help', 'display help for command')
-
+    .action(function() {
+        try{ 
+            let o1 = readfile(this.args[0]),
+            o2=readfile(this.args[1]);
+                console.error('Run script %s on port %s - %s -%s', this.args[0], 
+                    JSON.stringify(this.opts()),o1,o2);
+        } catch (err){
+            console.log (err.message);
+            return false;
+        }
+   });
 program.parse();
 const options = program.opts();
+if (options.debug)
+   console.log(options);
+
 if(options.help)
     program.outputHelp();
 
-if (options.debug)
-   console.log(options);
+// macOS, Linux, and Windows
+//readFileSync('<directory>');
