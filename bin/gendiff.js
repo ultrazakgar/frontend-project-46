69fa6fa32b61d8507abf85342ad2cb77f48035c0
ultrazakgar <zahariavinogr@gmail.com>
2026-02-12 19:56:14 +0300

    step 3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,51 +1,77 @@
-import { Command } from 'commander';
-import { readFileSync } from 'node:fs';
-import { accessSync, constants } from 'node:fs';
-const program = new Command();
+import { Command } from 'commander'
+import { readFileSync } from 'node:fs'
+import { accessSync, constants } from 'node:fs'
+const program = new Command()
 
-function readfile(filename){
-        let valid = true;
-        try{ // testing
-            accessSync(filename, constants.R_OK)
-        } catch (err){
-            valid = false;
-            console.log (err.message);
-            return null;
+function readfile(filename) {
+  try { // testing
+    accessSync(filename, constants.R_OK)
+  }
+  catch (err) {
+    console.log (err.message)
+    return null
+  }
+  return readFileSync(filename)
+}
+
+function compareObjects(a, b) {
+  let sum = [...new Set(Object.keys(a).concat(Object.keys(b)))].sort(), result = []
+  for (let i of sum) {
+    if (i in a) {
+      if (i in b) {
+        if (a[i] === b[i]) {
+          result.push({ sign: ' ', key: i, value: b[i] })
         }
-        return readFileSync(filename);
+        else {
+          result.push({ sign: '-', key: i, value: a[i] })
+          result.push({ sign: '+', key: i, value: b[i] })
+        }
+      }
+      else {
+        result.push({ sign: '-', key: i, value: a[i] })
+      }
+    }
+    else {
+      result.push({ sign: '+', key: i, value: b[i] })
+    }
+  }
+  return result;
 }
 
 program
-    .name('gendiff')
-    .description('Compares two configuration files and shows a difference.')
-    .version('0.0.1');
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1')
 
 program
-    .argument('[filepath1]')
-    .argument('[filepath2]')
-    //    .command('aa')
-    //    .description('Split a string into substrings and display as an array')
-    //.option('-V, --version', 'output the version number')
-    .option('-f, --format [type]','output format')
-    .option('-h, --help', 'display help for command')
-    .action(function() {
-        try{ 
-            let o1 = readfile(this.args[0]),
-            o2=readfile(this.args[1]);
-                console.error('Run script %s on port %s - %s -%s', this.args[0], 
-                    JSON.stringify(this.opts()),o1,o2);
-        } catch (err){
-            console.log (err.message);
-            return false;
-        }
-   });
-program.parse();
-const options = program.opts();
+  .argument('[filepath1]')
+  .argument('[filepath2]')
+//    .command('aa')
+//    .description('Split a string into substrings and display as an array')
+// .option('-V, --version', 'output the version number')
+  .option('-f, --format [type]', 'output format')
+  .option('-h, --help', 'display help for command')
+  .action(function () {
+    try {
+      let o1 = readfile(this.args[0]),
+        o2 = readfile(this.args[1])
+      // so let's out it
+      let result = compareObjects(JSON.parse(o1), JSON.parse(o2))
+      console.log('{')
+      for (let row of result) {
+        console.log('  %s %s: %s', row.sign, row.key, row.value)
+      }
+      console.log('}')
+    }
+    catch (err) {
+      console.log (err.message)
+      return false
+    }
+  })
+program.parse()
+const options = program.opts()
 if (options.debug)
-   console.log(options);
+  console.log(options)
 
-if(options.help)
-    program.outputHelp();
-
-// macOS, Linux, and Windows
-//readFileSync('<directory>');
+if (options.help)
+  program.outputHelp()
